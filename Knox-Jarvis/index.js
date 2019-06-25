93cfe045666bd62ff8e3382003efe68e05c9e79a
Knoxj1 <knoxjarvis@gmail.com>
2019-06-25 18:54:59 -0400

    Experimenting with different color scheme

--- Knox-Jarvis/index.js
+++ Knox-Jarvis/index.js
@@ -7,3 +7,84 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+window.onload=function() {
+const button = document.querySelector(".login-btn");
+
+button.addEventListener("mouseover" , (event) => {
+event.target.style.color = "white";
+});
+
+button.addEventListener("mouseout" , (event) => {
+event.target.style.color = "black";
+});
+
+
+
+
+
+const home = document.querySelector(".home");
+
+home.addEventListener("mouseover" , (event) => {
+
+event.target.style.backgroundColor = "black";
+event.target.style.color = "white";
+});
+
+home.addEventListener("mouseout" , (event) => {
+event.target.style.color = "black";
+event.target.style.backgroundColor = "white";
+});
+
+
+
+
+const calculator = document.querySelector(".calculator");
+
+calculator.addEventListener("mouseover" , (event) => {
+
+event.target.style.backgroundColor = "black";
+event.target.style.color = "white";
+});
+
+calculator.addEventListener("mouseout" , (event) => {
+event.target.style.color = "black";
+event.target.style.backgroundColor = "white";
+});
+
+
+
+
+
+const about = document.querySelector(".About");
+
+about.addEventListener("mouseover" , (event) => {
+
+event.target.style.backgroundColor = "black";
+event.target.style.color = "white";
+});
+
+about.addEventListener("mouseout" , (event) => {
+event.target.style.color = "black";
+event.target.style.backgroundColor = "white";
+});
+
+
+const contact = document.querySelector(".Contact");
+
+contact.addEventListener("mouseover" , (event) => {
+
+event.target.style.backgroundColor = "black";
+event.target.style.color = "white";
+});
+
+contact.addEventListener("mouseout" , (event) => {
+event.target.style.color = "black";
+event.target.style.backgroundColor = "white";
+});
+
+///carousel JS////
+
+
+
+
+};
